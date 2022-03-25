d17e2fb1bf13044853497aa413521b47985a452c
liam1902 <liam.jiang@makeblock.com>
2022-03-25 18:53:02 +0800

    feat: 发布1.0.4 包

--- src/app_channel.ts
+++ src/app_channel.ts
@@ -1,6 +1,6 @@
 import { Device } from "./enum";
 
-const _global: any = (window /* browser */ || global /* node */ ) as any;
+const _global: any = (window /* browser */ || global /* node */) as any;
 class AppChannel {
   public handleMessage: any = null;
   device: string = Device.auto;//"auto";//设备: auto , ios, android 3个值，默认auto
@@ -34,8 +34,8 @@ class AppChannel {
     this.handleMessage = null;
   }
 
-  callBack(v: string) {
-    console.log("callBackFromApp:", v);
+  callBack = (v: string) => {
+    console.log(":", v);
     if (this.handleMessage) {
       this.handleMessage(v);
     }
